d71ce07fa5221eab97e475927ec59a5db6937efa
Artem Makhov <makhovartem23@gmail.com>
2022-05-16 16:31:56 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsListEl = document.querySelector('#ingredients');
+
+const items = ingredients.map(item => {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = item;
+  return itemEl;
+});
+
+ingredientsListEl.append(...items);
+
+
+
