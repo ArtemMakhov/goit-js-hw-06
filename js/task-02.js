0744f9f9bfe895c1cc93319eb64d39f1c75cdb40
Artem Makhov <makhovartem23@gmail.com>
2022-05-16 17:05:32 +0300

    task-2-v2

--- js/task-02.js
+++ js/task-02.js
@@ -9,12 +9,17 @@ const ingredients = [
 
 const ingredientsListEl = document.querySelector('#ingredients');
 
-const items = ingredients.map(item => {
-  const itemEl = document.createElement('li');
-  itemEl.classList.add('item');
-  itemEl.textContent = item;
-  return itemEl;
-});
+const makeNewItems = ingredients => {
+  return ingredients.map(ingredient => {
+    const itemEl = document.createElement('li');
+    itemEl.classList.add('item');
+    itemEl.textContent = ingredient;
+    
+    return itemEl;
+  });
+};
+
+const items = makeNewItems(ingredients);
 
 ingredientsListEl.append(...items);
 
